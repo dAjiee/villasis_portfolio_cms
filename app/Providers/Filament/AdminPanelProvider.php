52f11d5a95b6d36002d41688d20431d10e373ce2
Alexander Villasis <alexandervaugn@gmail.com>
2024-05-24 06:31:30 +0800

    chore: Add email verification to admin panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -30,6 +30,7 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login()
             ->profile()
+            ->emailVerification()
             ->passwordReset()
             ->colors([
                 'primary' => Color::Amber,
